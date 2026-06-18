0a2701cd1a3b0bfb5e9ef57f3e7cfb037f24d0a6
Shreyash Bogar <shreyash.bogar@hcltech.com>
2026-06-18 15:14:36 +0530

    first commit

--- src/main/java/com/democheck/demo/service/TransactionService.java
+++
@@ -1,4 +0,0 @@
-package com.democheck.demo.service;
-
-public interface TransactionService {
-}
---
+++ src/main/java/com/democheck/demo/service/impl/TransactionService.java
@@ -0,0 +1,15 @@
+package com.democheck.demo.service;
+
+import com.democheck.demo.dto.TransactionHistoryResponse;
+import com.democheck.demo.dto.TransactionRequest;
+import com.democheck.demo.dto.TransactionResponse;
+
+import java.util.List;
+
+public interface TransactionService {
+    TransactionResponse createTransaction(
+            TransactionRequest request
+    );
+    TransactionResponse getTransaction(String transactionId);
+    List<TransactionHistoryResponse> getTransactionHistory(Long userId);
+}
